09852901755b6d7bb4b93d2f25d7c634cb0d7b32
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-17 10:56:46 -0300

    Transformando inputs em componentes controlados

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,13 @@
 import "./style.scss";
 
 import UFPELogo from "../../assets/images/login-page/UFPE-Logo.png";
+import { useState, } from "react";
 
 function Login()
 {
+  const [login, setLogin,] = useState<string>("",);
+  const [password, setPassword,] = useState<string>("",);
+
   return (
     <div className="login-page">
       <div className="login-container">
@@ -11,8 +15,20 @@ function Login()
 
         <h2 className="login-title">Vagas Internas</h2>
 
-        <input className="login" type="text" placeholder="Login"/>
-        <input className="password" type="password" placeholder="Senha"/>
+        <input
+          className="login"
+          type="text"
+          placeholder="Login"
+          value={login}
+          onChange={(e,) => setLogin(e.target.value,)}
+        />
+        <input
+          className="password"
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e,) => setPassword(e.target.value,)}
+        />
 
         <button className="login-button">Efetuar Login</button>
       </div>
