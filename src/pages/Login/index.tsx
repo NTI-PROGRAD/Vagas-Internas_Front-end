8bd20f6796f35828d551f2e9924b5b6ac61452da
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-17 10:48:07 -0300

    Implementando pagina de Login, versao desktop

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,21 @@
 import "./style.scss";
 
+import UFPELogo from "../../assets/images/login-page/UFPE-Logo.png";
+
 function Login()
 {
   return (
     <div className="login-page">
-      <h1>Login</h1>      
+      <div className="login-container">
+        <img className="ufpe-logo" src={UFPELogo} />
+
+        <h2 className="login-title">Vagas Internas</h2>
+
+        <input className="login" type="text" placeholder="Login"/>
+        <input className="password" type="password" placeholder="Senha"/>
+
+        <button className="login-button">Efetuar Login</button>
+      </div>
     </div>
   );
 }
